ed8ff2e5f46bf79d5afb6eabf8cd6834bfbe83a6
DenisaNavratilova <deniska.navratilova13@gmail.com>
2024-01-07 13:23:05 +0100

    Navratilova

---
+++ Navratilova/Navratilova/Model/Credits.cs
@@ -0,0 +1,10 @@
+﻿namespace Navratilova.Model
+{
+    public class Credits
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public int Value { get; set; }
+        public bool Success { get; set; }
+    }
+}
---
+++ Navratilova/Navratilova/Model/CreditsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Navratilova.Model;
+using System.Collections.Generic;
+
+namespace Navratilova.Model
+{
+    public class CreditsContext : DbContext
+    {
+        public DbSet<Credits> Credits { get; set; }
+
+        public CreditsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
