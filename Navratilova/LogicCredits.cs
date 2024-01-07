5d62d8c88b9060280e580704681bfd4501e0d180
DenisaNavratilova <deniska.navratilova13@gmail.com>
2024-01-07 13:27:40 +0100

    semester1

---
+++ Navratilova/LogicCredits.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Navratilova;
+using Navratilova.Model;
+
+namespace Navratilova
+{
+    public class LogicCredits
+    {
+        private readonly CreditsContext _dbContext;
+
+        public LogicCredits(CreditsContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddCredit(int value, bool success)
+        {
+            var credit = new Credits
+            {
+                Created = DateTime.Now,
+                Value = value,
+                Success = success
+            };
+
+            _dbContext.Credits.Add(credit);
+            _dbContext.SaveChanges();
+        }
+    }
+}
